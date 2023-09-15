466290abc3e43d9c2dea2eb882276e0b25b1cde6
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-09-15 13:31:59 -0700

    sort of works for N > 1

--- Words/wfc_words_overlapping.py
+++ Words/wfc_words_overlapping.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import random
 import copy
 
-N: int = 1
-FILENAME = "book-database/alice.txt"
+N: int = 2
+FILENAME = "book-database/test.txt"
 OUTPUT_LENGTH = 10
 
 class Word:
@@ -39,6 +39,11 @@ class Spot:
         self.words = [self.random_weighted_word()]
         self.collapsed = True
 
+    def collapse_to_period(self):
+        period_words = [word for word in self.words if word.word == "."]
+        self.words = [period_words[0]]
+        self.collapsed = True
+
     # def update(self, left_words: list[Word] | None = None, right_words: list[Word] | None = None) -> bool:
     #     previous_count = len(self.words)
 
@@ -64,9 +69,16 @@ class Spot:
         offset = -offset
 
         neighbor_word_strings = [word.word for word in neighbor_words]
-        # print(neighbor_word_strings[:5])
-        # print(self.words[0].allowed[offset][:5])
-        self.words = [word for word in self.words if len([True for allowed_word in word.allowed[offset] if allowed_word in neighbor_word_strings]) > 0]
+        # self.words = [word for word in self.words if len([True for allowed_word in word.allowed[offset] if allowed_word in neighbor_word_strings]) > 0]
+
+        new_words = []
+        for word in self.words:
+            # if len([True for allowed_word in word.allowed[offset] if allowed_word in neighbor_word_strings]) > 0:
+            #     new_words.append(word)
+            if offset in word.allowed.keys():
+                if len([True for allowed_word in word.allowed[offset] if allowed_word in neighbor_word_strings]) > 0:
+                    new_words.append(word)
+        self.words = new_words
 
         if len(self.words) == 1:
             self.collapsed = True
@@ -101,12 +113,12 @@ def propagate(spots: list[Spot], index: int) -> None:
         offsets = [i for i in range(-N, N + 1) if i != 0]
         neighbor_idxs = [current_index + offset for offset in offsets]
 
+        current_words = spots[current_index].words
+
         for neighbor_idx, offset in zip(neighbor_idxs, offsets):
             if neighbor_idx <= 0 or neighbor_idx >= len(spots) - 1:
                 continue
 
-            # neighbor_words = spots[neighbor_idx].words
-            current_words = spots[current_index].words
             updated = spots[neighbor_idx].update(current_words, offset)
 
             if updated and neighbor_idx not in stack:
@@ -188,50 +200,103 @@ def read_words(filename: str) -> tuple[list[Word], list[str]]:
 
     left_word = "."
     current_word = filtered_words[0]
-    right_word = filtered_words[1]
-    all_word_combs: list[tuple[str, str, str]] = [(current_word, left_word, right_word)]
+    word_combo = (current_word, {-1: [left_word]})
+    all_word_combs: list[tuple[str, dict[int, list[str]]]] = [word_combo]
 
-    for i, current_word in enumerate(filtered_words[1:]):
-        i += 1
+    # left_word = "."
+    # current_word = filtered_words[0]
+    # right_word = filtered_words[1]
+    # all_word_combs: list[tuple[str, str, str]] = [(current_word, left_word, right_word)]
+    offsets = [i for i in range(-N, N + 1) if i != 0]
 
-        left_word = filtered_words[i - 1]
-        try:
-            right_word = filtered_words[i + 1]
-        except IndexError:
-            right_word = "."
+    print(filtered_words)
 
-        tup = (current_word, left_word, right_word)
-        all_word_combs.append(tup)
+    for i in range(len(filtered_words)):
+        current_word = filtered_words[i]
+
+        neighbor_idxs = [i + offset for offset in offsets]
+        for neighbor_idx, offset in zip(neighbor_idxs, offsets):
+            if neighbor_idx < -1 or neighbor_idx > len(filtered_words):
+                continue
+
+            if neighbor_idx == len(filtered_words) or neighbor_idx == -1:
+                offset_word = "."
+            else:
+                offset_word = filtered_words[neighbor_idx]
+            
+            tup = (current_word, {offset: [offset_word]})
+
+            if current_word == "hardly":
+                print(tup)
+
+            all_word_combs.append(tup)
+
+        # left_word = filtered_words[i - 1]
+        # try:
+        #     right_word = filtered_words[i + 1]
+        # except IndexError:
+        #     right_word = "."
+
+        # tup = (current_word, left_word, right_word)
+        # all_word_combs.append(tup)
 
     
     words: list[Word] = []
     for word_comb in all_word_combs:
-        if word_comb[0] not in [word.word for word in words]:
-            allowed = {
-                -1: [word_comb[1]],
-                1: [word_comb[2]]
-            }
-            words.append(Word(word_comb[0], 1, allowed))
+        word_str = word_comb[0]
+        allowed = word_comb[1]
+
+        if word_str not in [word.word for word in words]:
+            words.append(Word(word_str, 1, allowed))
         else:
             for word in words:
-                if word.word == word_comb[0]:
+                if word.word == word_str:
                     word.word_weight += 1
 
-                    if word_comb[1] not in word.allowed[-1]:
-                        word.allowed[-1].append(word_comb[1])
-                    if word_comb[2] not in word.allowed[1]:
-                        word.allowed[1].append(word_comb[2])
+                    offset = list(allowed.keys())[0]
+                    allowed_word = allowed[offset][0]
+
+                    if offset not in word.allowed.keys():
+                        word.allowed[offset] = []
+                    if allowed_word not in word.allowed[offset]:
+                        word.allowed[offset].append(allowed_word)
+                    
+
+                    # if word_comb[1] not in word.allowed[-1]:
+                    #     word.allowed[-1].append(word_comb[1])
+                    # if word_comb[2] not in word.allowed[1]:
+                    #     word.allowed[1].append(word_comb[2])
+
+
     word_strs = [word.word for word in words]
 
     return words, word_strs
 
-def create_spots(words: list[Word], length: int) -> list[Spot]:
+def create_spots(words: list[Word], length: int, word_strs: list[str]) -> list[Spot]:
     spots: list[Spot] = []
     for _ in range(length):
         spot = Spot()
         for word in words:
             spot.add_word(copy.deepcopy(word))
         spots.append(spot)
+
+    # start_word = Word(".", 1, {1: word_strs})
+    # spots[0].words = [start_word]
+    # spots[0].collapse()
+
+    # end_word = Word(".", 1, {-1: word_strs})
+    # spots[-1].words = [end_word]
+    # spots[-1].collapse()
+
+    spots[0].collapse_to_period()
+    spots[-1].collapse_to_period()
+
+    # propagate(spots, 0)
+    # print(join_spots(spots))
+
+    propagate(spots, len(spots) - 1)
+    print(join_spots(spots))
+
     return spots
 
 def join_spots(spots: list[Spot]) -> str:
@@ -256,27 +321,29 @@ def main() -> None:
     words, word_strs = read_words(FILENAME)
     print("Words read")
 
-    spots: list[Spot] = []
-    for _ in range(OUTPUT_LENGTH):
-        spot = Spot()
-        for word in words:
-            spot.add_word(copy.deepcopy(word))
-        spots.append(spot)
+    spots = create_spots(words, OUTPUT_LENGTH, word_strs)
+
+    # spots: list[Spot] = []
+    # for _ in range(OUTPUT_LENGTH):
+    #     spot = Spot()
+    #     for word in words:
+    #         spot.add_word(copy.deepcopy(word))
+    #     spots.append(spot)
 
 
-    start_word = Word(".", 1, {-1: word_strs})
-    spots[0].words = [start_word]
-    spots[0].collapse()
+    # start_word = Word(".", 1, {-1: word_strs})
+    # spots[0].words = [start_word]
+    # spots[0].collapse()
 
-    end_word = Word(".", 1, {1: word_strs})
-    spots[-1].words = [end_word]
-    spots[-1].collapse()
+    # end_word = Word(".", 1, {1: word_strs})
+    # spots[-1].words = [end_word]
+    # spots[-1].collapse()
 
-    propagate(spots, 0)
-    print(join_spots(spots))
+    # propagate(spots, 0)
+    # print(join_spots(spots))
 
-    propagate(spots, len(spots) - 1)
-    print(join_spots(spots))
+    # propagate(spots, len(spots) - 1)
+    # print(join_spots(spots))
 
     done = False
     while not done:
@@ -287,7 +354,8 @@ def main() -> None:
 
         if failed:
             print("KNOTTED, RESTARTING\n")
-            spots = create_spots(words, OUTPUT_LENGTH)
+            spots = create_spots(words, OUTPUT_LENGTH, word_strs)
+            break
     
 
 
